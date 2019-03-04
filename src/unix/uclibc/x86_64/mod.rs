d938722e65ce1a2e638582152ef124a214e121db
Sebastian Humenda <shumenda@gmx.de>
2019-03-04 10:05:27 +0100

    l4re/uclibc: derive Debug for pthread_addr_t

--- src/unix/uclibc/x86_64/mod.rs
+++ src/unix/uclibc/x86_64/mod.rs
@@ -34,19 +34,6 @@ s! {
         __unused2: ::c_ulong
     }
 
-    #[cfg(not(target_os = "l4re"))]
-    pub struct pthread_attr_t {
-        __detachstate: ::c_int,
-        __schedpolicy: ::c_int,
-        __schedparam: __sched_param,
-        __inheritsched: ::c_int,
-        __scope: ::c_int,
-        __guardsize: ::size_t,
-        __stackaddr_set: ::c_int,
-        __stackaddr: *mut ::c_void, // better don't use it
-        __stacksize: ::size_t,
-    }
-
     pub struct __sched_param {
         __sched_priority: ::c_int,
     }
--- src/unix/uclibc/x86_64/other.rs
+++ src/unix/uclibc/x86_64/other.rs
@@ -3,3 +3,17 @@
 pub type pthread_t = ::c_ulong;
 
 pub const PTHREAD_STACK_MIN: usize = 16384;
+
+s! {
+    pub struct pthread_attr_t {
+        __detachstate: ::c_int,
+        __schedpolicy: ::c_int,
+        __schedparam: __sched_param,
+        __inheritsched: ::c_int,
+        __scope: ::c_int,
+        __guardsize: ::size_t,
+        __stackaddr_set: ::c_int,
+        __stackaddr: *mut ::c_void, // better don't use it
+        __stacksize: ::size_t,
+    }
+}
